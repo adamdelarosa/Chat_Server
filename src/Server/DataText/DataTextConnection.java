1212a3baca322527fc102c9a9ce2c32bb50ffae1
ROSA <gojiradam@gmail.com>
2016-03-28 19:47:45 +0200

    Database Strings

--- src/Server/DataText/DataTextConnection.java
+++ src/Server/DataText/DataTextConnection.java
@@ -36,6 +36,46 @@ public class DataTextConnection {
         Connection conn = null;
         Statement stmt = null;
         System.out.println("Hello");
-    }
 
+        try{
+            //STEP 2: Register JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(USERNAME,PASSWORD,DATABASE_URL);
+
+            //STEP 4: Execute a query
+            System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT id, first, last, age FROM Employees";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            //STEP 5: Extract data from result set
+            while(rs.next()){
+                //Retrieve by column name
+                int id  = rs.getInt("id");
+                int age = rs.getInt("age");
+                String first = rs.getString("first");
+                String last = rs.getString("last");
+
+                //Display values
+                System.out.print("ID: " + id);
+                System.out.print(", Age: " + age);
+                System.out.print(", First: " + first);
+                System.out.println(", Last: " + last);
+            }
+            //STEP 6: Clean-up environment
+            rs.close();
+            stmt.close();
+            conn.close();
+
+        }catch(SQLException se){
+            se.printStackTrace();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+            System.out.println("Connection MYSQL CLOSED.");
+        }
 }
