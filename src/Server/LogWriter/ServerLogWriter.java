e13891da5cc9467185365e2de034c80e9392e6b2
ROSA <gojiradam@gmail.com>
2016-03-28 19:15:39 +0200

    Database Strings

--- src/Server/LogWriter/ServerLogWriter.java
+++ src/Server/LogWriter/ServerLogWriter.java
@@ -1,7 +1,35 @@
 package Server.LogWriter;
 
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 /**
  * Created by ROSA on 3/28/16.
  */
+
 public class ServerLogWriter {
+
+    Logger logger = Logger.getLogger("MyLog");
+    FileHandler fileHandler;
+
+    public void serverLog() {
+
+        try {
+
+            fileHandler = new FileHandler("./../server.log");
+            logger.addHandler(fileHandler);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fileHandler.setFormatter(formatter);
+
+            logger.info("Message");
+
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        logger.info("INFO");
+    }
 }
