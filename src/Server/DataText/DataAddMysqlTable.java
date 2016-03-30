3010556a0a0bc454ac6e3a0af508e10f0568fe70
ROSA <gojiradam@gmail.com>
2016-03-30 19:27:01 +0200

    Database connection

---
+++ src/Server/DataText/DataAddMysqlTable.java
@@ -0,0 +1,70 @@
+package Server.DataText;
+
+import Server.Controller;
+import java.sql.*;
+import java.util.Calendar;
+/**
+ * Created by ROSA on 3/28/16.
+ */
+
+public class DataAddMysqlTable {
+
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DATABASE_URL = "jdbc:mysql://localhost/TEXTDATA";
+    static final String USERNAME = "username";
+    static final String PASSWORD = "password";
+    static String textMessageData = "Hello again";
+
+    public void mysqlConnectionAddTable(){
+        Connection conn = null;
+        Statement stmt = null;
+
+        Calendar calendar = Calendar.getInstance();
+        java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
+        java.sql.Time stertTime = new java.sql.Time(calendar.getTime().getTime());
+
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+            stmt = conn.createStatement();
+            String sql = null;
+
+            //Create DB:
+            /*sql = "CREATE DATABASE TEXTDATA";
+            stmt.executeUpdate(sql);*/
+
+            //Create Table:
+            /*sql = "CREATE TABLE TEXT_DATA_TABLE " + "(DATE DATE ,TIME TIME , TEXT BLOB(1000))";
+            stmt.executeUpdate(sql);*/
+
+            //ADD TEXT DATA IN REAL - TIME.
+            String query = " insert into TEXT_DATA_TABLE (DATE , TIME , TEXT)" + " values (?,?,?)";
+
+            PreparedStatement preparedStmt = conn.prepareStatement(query);
+            preparedStmt.setDate (1, startDate);
+            preparedStmt.setTime (2, stertTime);
+            preparedStmt.setString (3, textMessageData);
+            preparedStmt.execute();
+            System.out.println(startDate);
+
+            System.out.println("Database.");
+
+        }catch(SQLException se){
+            se.printStackTrace();
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            try{
+                if(stmt!=null)
+                    conn.close();
+            }catch(SQLException se){
+            }
+            try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }
+        }
+    }
+}
